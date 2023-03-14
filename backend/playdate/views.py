511c98f363f6ee42df0074ed8666364460dfe118
Nada Atieh <nada.s.atieh@gmail.com>
2023-03-14 12:16:57 -0500

    Added permission class to protect end points

--- backend/playdate/views.py
+++ backend/playdate/views.py
@@ -1,26 +1,28 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from .models import PlayDate
 from .serializers import PlayDateSearializer
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # Create your views here.
 
-@api_view(['GET', 'POST', 'DELETE']) 
-def getdate(request):
 
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def getdate(request):
     if request.method == 'GET':
 
         zipcode = request.query_params.get('zipcode')
 
-        dates= PlayDate.objects.all()
-        
-        if zipcode: 
+        dates = PlayDate.objects.all()
+
+        if zipcode:
             dates.filter(zipcode=zipcode)
 
         serializer = PlayDateSearializer(dates, many=True)
-        return Response (serializer.data)
+        return Response(serializer.data)
     elif request.method == 'POST':
         serializer = PlayDateSearializer(data=request.data)
         if serializer.is_valid() == True:
@@ -31,17 +33,18 @@ def getdate(request):
     elif request.method == 'DELETE':
         PlayDate.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-    
-@api_view(['GET'])
-def playdate_zipcode(request,zipcode):
 
-        playdates = PlayDate.objects.filter(zipcode=zipcode)
-        serializer = PlayDateSearializer(playdates, many=True)
-        return Response(serializer.data)
 
-    
 @api_view(['GET'])
-def surrounding_playdates(request,pk):
+def playdate_zipcode(request, zipcode):
+
+    playdates = PlayDate.objects.filter(zipcode=zipcode)
+    serializer = PlayDateSearializer(playdates, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def surrounding_playdates(request, pk):
 
     try:
         playdate = PlayDate.objects.get(pk=pk, )
