c5ae63b1bd4d793a7eab278678b4cbd6640455d1
Nada Atieh <nada.s.atieh@gmail.com>
2023-03-30 12:05:42 -0500

    Created a Post request in dogs views file + Added path for dogs POST request in dogs urls file and project urls file

--- backend/dogs/urls.py
+++ backend/dogs/urls.py
@@ -1,8 +1,8 @@
-from . import views
+from dogs import views
 from django.urls import path, include
 
 
 urlpatters = [
     path('<int:id>/', views.dog_id),
-    path('', views.dog_id)
+    path('', views.dog_id),
 ]
